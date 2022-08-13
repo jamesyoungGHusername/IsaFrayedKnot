40ebeb266a1b624e4d01086df33499809a46e90f
Athena Petrovich <athenal.petrovich@gmail.com>
2022-08-12 21:52:57 -0600

    Knockback works :D
    fixed knockback successfully

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -7,9 +7,9 @@ public class Knockback : MonoBehaviour
     public float hit;
     public float knockTime;
 
-    private void OnCollisionEnter(Collider2D collision){
-        if (collision.gameObject.CompareTag("enemy")){
-            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Enemy")){
+            Rigidbody2D enemy = collision.gameObject.GetComponent<Rigidbody2D>();
             if (enemy != null){
                 Vector2 difference = enemy.transform.position - transform.position;
                 difference = difference.normalized * hit;
